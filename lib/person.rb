003baa5ee4a994b02ee91c6ac4dfca8b52caae4a
Joshua Nguyen-Cabral <joshycsm@gmail.com>
2019-11-25 00:44:31 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,6 +5,12 @@ class Person
   end
 end
 
+<<<<<<< HEAD
 p Peter = Person.new
 # p Peter.name = "Peter"
 p Peter.name
+=======
+p Peter = Person.new("John")
+p Peter 
+p peter.name
+>>>>>>> 5fb830b86ef7ff1cc5ebe4080e924720d9812c11
